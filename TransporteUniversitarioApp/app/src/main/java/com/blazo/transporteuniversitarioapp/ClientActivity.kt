0d6e53e51de944c8d8ee16ca098cdc57adad87bd
riveraalejandro1999 <54332002+riveraalejandro1999@users.noreply.github.com>
2020-05-27 16:10:46 -0600

    Vsitas cliente segunda parte

--- TransporteUniversitarioApp/app/src/main/java/com/blazo/transporteuniversitarioapp/ClientActivity.kt
+++ TransporteUniversitarioApp/app/src/main/java/com/blazo/transporteuniversitarioapp/ClientActivity.kt
@@ -46,7 +46,7 @@ class ClientActivity : AppCompatActivity() {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_gallery,R.id.nav_map), drawerLayout)
+                R.id.nav_home, R.id.nav_gallery,R.id.nav_map,R.id.nav_ajustes), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
